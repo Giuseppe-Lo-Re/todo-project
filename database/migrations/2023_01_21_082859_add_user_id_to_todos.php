6fe76a5570145d5b46219bf786e32d31abe76943
Giuseppe-lore <loregiuseppe0@gmail.com>
2023-01-21 09:38:43 +0100

    added relation between users and todos

---
+++ database/migrations/2023_01_21_082859_add_user_id_to_todos.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTodos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+
+            // Add user_id table
+            $table->unsignedBigInteger('user_id')->nullable(); 
+
+            // Add relation
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            
+            // Delete relation
+            $table->dropForeign('todos_user_id_foreign';)
+
+            // Delete user_id table
+            $table->dropColumn('user_id');
+        });
+    }
+}
